fa9a4add0328a3b5e4b07c4e90ac9bdcb8d39e6b
codels <codels25@gmail.com>
2015-06-25 00:14:13 +0300

    send_message part 2

--- send_message.php
+++ send_message.php
@@ -1,5 +1,8 @@
 <?php
 
+date_default_timezone_set('Europe/Moscow');
+mb_internal_encoding('UTF-8');
+
 include_once('config.php');
 
 function _request($token, $command, $params = null)
@@ -11,17 +14,24 @@ function _request($token, $command, $params = null)
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_POST => true,
         CURLOPT_HEADER => false,
-        CURLOPT_HTTPHEADER => array('Host: api.telegram.org'),
-        CURLOPT_POSTFIELDS => json_encode($params),
+        CURLOPT_HTTPHEADER => array(
+            'Host: api.telegram.org',
+            'Content-Type: application/x-www-form-urlencoded'
+        ),
+        CURLOPT_POSTFIELDS => $params ? http_build_query($params) : null,
         CURLOPT_TIMEOUT => 0,
         CURLOPT_CONNECTTIMEOUT => 600,
         CURLOPT_SSL_VERIFYPEER => false
     ));
 
     $body = curl_exec($resource);
-    //$errorNumber = curl_errno($resource);
-    //$errorMessage = curl_error($resource);
-    //var_dump(array($errorMessage, $errorNumber));
+
+    $errorNumber = curl_errno($resource);
+    if ($errorNumber) {
+        $errorMessage = curl_error($resource);
+        error_log("error #{$errorNumber}: {$errorMessage}");
+    }
+
     curl_close($resource);
     if (empty($body)) {
         return null;
@@ -34,3 +44,5 @@ function _request($token, $command, $params = null)
 }
 
 var_dump(_request($token, 'getUpdates'));
+echo '<br><br>';
+//var_dump(_request($token, 'sendMessage', array('chat_id'=>$chatId, 'text'=>'test message')));
